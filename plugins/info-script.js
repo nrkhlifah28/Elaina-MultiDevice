00fbdfbbb24141e7d3e2a55c04d7baeed9de49c3
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-10-02 12:06:06 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,12 +4,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `*Apa? SC???Cari sendiri lah modal!!*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
-    title: 'Elaina-MultiDevice',
+    mediaUrl: "https://Instagram.com/Nrkhlifah28",
+    title: 'NoerBoTZ-Mdོ',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
