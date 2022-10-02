1ea7259f4cc82e5175b626e642b18a5cbc9410c3
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-10-02 11:52:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Indosat:* [${global.ppulsa}]
+│ • *Trheeee 3:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
